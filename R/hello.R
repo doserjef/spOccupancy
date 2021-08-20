05e44ab497bb9c2d36ba17ddb058179ceeecc424
doserjef <doserjef@msu.edu>
2021-08-20 10:07:39 -0400

    Initial setup

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,10 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
+#' Print Hello World
+#'
+#' Print a nice message for testing.
+#'
+#' @export
+#' @examples
+#' hello()
 hello <- function() {
   print("Hello, world, part 2!")
 }
