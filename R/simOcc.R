56221e09a69f2a89ce20555e7d86e9264e994916
doserjef <doserjef@msu.edu>
2021-08-20 11:18:29 -0400

    simOcc

--- R/simOcc.R
+++ R/simOcc.R
@@ -1,3 +1,27 @@
+#' Simulate Single-Species Occupancy Data
+#'
+#' @param J.x Number of cells on x-axis
+#' @param J.y Number of cells on y-axis
+#' @param K Number of repeat visits
+#' @param beta Occupancy covariate values
+#' @param alpha Detection covariate values
+#' @param sigma.sq Spatial variance parameter
+#' @param phi Spatial range parameter
+#' @param sp Logical indicating if simulate spatial model or not.
+#'
+#' @return stuff
+#' @export
+#'
+#' @examples J.x <- 10
+#'J.y <- 10
+#'K <- rep(4, J.x * J.y)
+#'beta <- c(0.5, -0.15)
+#'alpha <- c(0.7, 0.4)
+#'phi <- 3 / .6
+#'sigma.sq <- 2
+#'dat <- simOcc(J.x = J.x, J.y = J.y, K = K, beta = beta, alpha = alpha,
+#'              sigma.sq = sigma.sq, phi = phi, sp = TRUE)
+
 simOcc <- function(J.x, J.y, K, beta, alpha, sigma.sq = 2, phi = 3/0.5,
                    sp = FALSE) {
 
@@ -8,7 +32,7 @@ simOcc <- function(J.x, J.y, K, beta, alpha, sigma.sq = 2, phi = 3/0.5,
     if(any(is.na(match(dim(V),p))))
       stop("Dimension problem!")
     D <- chol(V)
-    t(matrix(rnorm(n*p), ncol=p)%*%D + rep(mu,rep(n,p)))
+    t(matrix(stats::rnorm(n*p), ncol=p)%*%D + rep(mu,rep(n,p)))
   }
 
   logit <- function(theta, a = 0, b = 1){log((theta-a)/(b-theta))}
@@ -20,7 +44,7 @@ simOcc <- function(J.x, J.y, K, beta, alpha, sigma.sq = 2, phi = 3/0.5,
   X.psi <- matrix(1, nrow = J, ncol = n.beta)
   if (n.beta > 1) {
     for (i in 2:n.beta) {
-      X.psi[, i] <- rnorm(J)
+      X.psi[, i] <- stats::rnorm(J)
     } # i
   }
 
@@ -31,7 +55,7 @@ simOcc <- function(J.x, J.y, K, beta, alpha, sigma.sq = 2, phi = 3/0.5,
   if (n.alpha > 1) {
     for (i in 2:n.alpha) {
       for (j in 1:J) {
-        X.p[j, 1:K[j], i] <- rnorm(K[j])
+        X.p[j, 1:K[j], i] <- stats::rnorm(K[j])
       } # j
     } # i
   }
@@ -42,7 +66,7 @@ simOcc <- function(J.x, J.y, K, beta, alpha, sigma.sq = 2, phi = 3/0.5,
   s.y <- seq(0, 1, length.out = J.y)
   coords <- expand.grid(s.x, s.y)
   # Distance matrix
-  D <- as.matrix(dist(coords))
+  D <- as.matrix(stats::dist(coords))
   # Exponential correlation function
   R <- exp(-phi * D)
   # Random spatial process
@@ -54,14 +78,14 @@ simOcc <- function(J.x, J.y, K, beta, alpha, sigma.sq = 2, phi = 3/0.5,
   } else {
     psi <- logit.inv(X.psi %*% as.matrix(beta))
   }
-  z <- rbinom(J, 1, psi)
+  z <- stats::rbinom(J, 1, psi)
 
   # Data Formation --------------------------------------------------------
   p <- matrix(NA, nrow = J, ncol = max(K))
   y <- matrix(NA, nrow = J, ncol = max(K))
   for (j in 1:J) {
     p[j, 1:K[j]] <- logit.inv(X.p[j, 1:K[j], ] %*% as.matrix(alpha))
-    y[j, 1:K[j]] <- rbinom(K[j], 1, p[j, 1:K[j]] * z[j])
+    y[j, 1:K[j]] <- stats::rbinom(K[j], 1, p[j, 1:K[j]] * z[j])
   } # j
 
   return(
